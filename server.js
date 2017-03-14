7fa6a1ebaadc7a77b5c53c0f089461f596de1c00
Priyanka Jagga <pjagga@Priyankas-MacBook-Pro.local>
2017-03-15 10:08:21 +1100

    Initial Commit

---
+++ server.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method}: ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if(err){
+      console.log('Unable to append the file');
+    }
+  });
+  next();
+});
+
+//Express middleware
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs', {
+//     pageTitle: 'Home Page',
+//     message: 'Site is under maintenance'
+//   });
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('upperCaseIt', (text) => {
+  return text.toUpperCase();
+});
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    name: 'Pri',
+    hobbies: [
+      'swimming',
+      'talking',
+      'eating'
+    ]
+  })
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    error: 404
+  })
+});
+app.listen(3000);
